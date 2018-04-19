bafef4b92211c056b1df7086dfbce25bf41fdda1
Checo <sergio_eduardosandoval@outlook.es>
2018-04-19 15:52:59 -0500

    Segundo programa de ng-repeat

--- js/app.js
+++ js/app.js
@@ -4,8 +4,25 @@
   var app= angular.module('UniversodadApp',[]);
 
   app.controller('listadoCtrl',['$scope',function($scope){
+    //objeto que tiene un arreglo de objetos
+    $scope.listadoProfesores={
+              profesores: [
+                {
+                nombre:"Fernando Herrera",
+                edad:34,
+                clase:"PEE"
+              },{
+                nombre:"Carlos Herrera",
+                edad:48,
+                clase:"ICE"
+              },{
+                nombre:"Gonzalo Herrera",
+                edad:78,
+                clase:"POO"
+              }
+            ]
 
-    $scope.listado=["México","Paris","Colombia","Alemania "];
+    }
 
   }]);
 

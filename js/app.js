a700875964cec428a750242d083dd61f159d4249
Checo <sergio_eduardosandoval@outlook.es>
2018-04-19 15:21:18 -0500

    Primer progrma con ng-repeat

--- js/app.js
+++ js/app.js
@@ -1,27 +1,12 @@
-var app= angular.module('UniversodadApp',[]);
-
-app.controller('profesorCtrl',function($scope){
-      $scope.profesor= profesorData;
-      $scope.editando={};
-      $scope.Mostrar=false;
-      $scope.EditarProfesor= function (){
-        angular.copy($scope.profesor, $scope.editando);
-        $scope.Mostrar=true;
-      }
-      $scope.GuardarCmabios=function(){
-        angular.copy($scope.editando, $scope.profesor);
-        $scope.Mostrar=false;
-      }
-      $scope.CancelarCambios=function(){
-         $scope.editando={};
-         $scope.Mostrar=false;
-      }
 
 
-})
-var profesorData = {
-	nombre: "Juan Carlos Pineda",
-	bio: "Saludos estudiante, mi nombre es Juan Carlos, encantado de conocerte, soy una apasionado instructor de matemáticas aplicadas cuánticas, más orientado a la física termonuclear. Mi vocación es ser maestro y lograr transmitir mis conocimientos a todos mis estudiantes!.",
-	edad: 47,
-	foto: "img/juancarlos.jpg"
-}
+(function(){
+  var app= angular.module('UniversodadApp',[]);
+
+  app.controller('listadoCtrl',['$scope',function($scope){
+
+    $scope.listado=["México","Paris","Colombia","Alemania "];
+
+  }]);
+
+})();

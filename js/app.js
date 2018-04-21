f5725a24752c20b301c00ef5252c452f105cf484
Checo <sergio_eduardosandoval@outlook.es>
2018-04-20 22:30:42 -0500

    creacion de las rutas y el ng-class

--- js/app.js
+++ js/app.js
@@ -1,38 +1,19 @@
-(function(){
+var app = angular.module('universidadApp',['ngRoute']);
 
-var app = angular.module('universidadApp',[ ]);
+app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
+//CONTROLADOR GENERAL
+  $scope.menuSuperior = "parciales/menu.html";
 
+  $scope.setActive=function(OpcionMenu){
 
+    $scope.mInicio ="";
+    $scope.mProfesores ="";
+    $scope.mAlumnos ="";
 
-app.controller('listadoCtrl', ['$scope', function($scope){
- $scope.paisSeleccionado="";
+    $scope[OpcionMenu]="active";
 
-  $scope.paises = [
-      { id:"CRI", nombre:"COSTA RICA"},
-      { id:"HRV", nombre:"CROACIA"},
-      { id:"CUB", nombre:"CUBA"},
-      { id:"DNK", nombre:"DINAMARCA"},
-      { id:"DMA", nombre:"DOMINICA"},
-      { id:"DOM", nombre:"REPÚBLICA DOMINICANA"},
-      { id:"ECU", nombre:"ECUADOR"},
-      { id:"EGY", nombre:"EGIPTO"},
-      { id:"SLV", nombre:"EL SALVADOR"},
-      { id:"ARE", nombre:"EMIRATOS ÁRABES UNIDOS"},
-      { id:"ERI", nombre:"ERITREA"},
-      { id:"SVK", nombre:"ESLOVAQUIA"},
-      { id:"SVN", nombre:"ESLOVENIA"},
-      { id:"ESP", nombre:"ESPAÑA"},
-      { id:"USA", nombre:"ESTADOS UNIDOS"},
-      { id:"EST", nombre:"ESTONIA"}
-  ];
-
+  }
 
 
 
 }]);
-
-
-
-
-
-})();
